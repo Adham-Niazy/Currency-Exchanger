a1ecb51841ca5157285090f6ed023eca9c17878a
Adham-Niazy <adhamniazy@gmail.com>
2024-01-08 08:45:53 +0200

    Dropdown Shared Component

---
+++ src/app/shared/components/dropdowns/dropdowns.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DropdownsComponent } from './dropdowns.component';
+
+describe('DropdownsComponent', () => {
+  let component: DropdownsComponent;
+  let fixture: ComponentFixture<DropdownsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DropdownsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DropdownsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
