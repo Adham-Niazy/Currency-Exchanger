9891356c4ac05e481690a206e280568b2f563a55
Adham-Niazy <adhamniazy@gmail.com>
2024-01-08 08:45:53 +0200

    Dropdown Shared Component

---
+++ src/app/shared/components/dropdowns/dropdowns.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CurrencySymbols } from 'src/app/core/models';
+
+@Component({
+  selector: 'app-dropdowns',
+  templateUrl: './dropdowns.component.html',
+  styleUrls: ['./dropdowns.component.scss']
+})
+export class DropdownsComponent implements OnInit {
+  @Input() control: FormControl = new FormControl();
+  @Input() list: CurrencySymbols = {};
+  @Input() placeholder = '';
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
