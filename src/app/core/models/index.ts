aedb40194b0f3f0fcb1697b1ddf1a4dbd0def1f2
Adham-Niazy <adhamniazy@gmail.com>
2024-01-08 21:12:22 +0200

    Adding new Model

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -1,3 +1,13 @@
-export interface CurrencySymbols {
+export type CurrencySymbols = {
   [symbol: string]: string;
+}
+
+export type ExchangeRateResponse = {
+  base: string;
+  date: string;
+  rates: {
+    [currencyCode: string]: number;
+  };
+  success: boolean;
+  timestamp: number;
 }
