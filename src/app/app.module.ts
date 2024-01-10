d0f3414c77f771c3a060ca81a54c7daebb634c7c
Adham-Niazy <adhamniazy@gmail.com>
2024-01-10 04:31:00 +0200

    Adjust Global Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './pages/home/home.component';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DetailsComponent } from './pages/details/details.component';
+import { MostPopularCurrenciesComponent } from './components/most-popular-currencies/most-popular-currencies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsComponent,
+    MostPopularCurrenciesComponent
   ],
   imports: [
     BrowserModule,
