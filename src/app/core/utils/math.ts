0df075c970a74fa5dea61eacfaff1d664bb78038
Adham-Niazy <adhamniazy@gmail.com>
2024-01-10 04:30:30 +0200

    Adding New Method to Calculate Inverses of rates

--- src/app/core/utils/math.ts
+++ src/app/core/utils/math.ts
@@ -1,11 +1,14 @@
-export function multiplyNumbers(numbers: number[]) {
-  return numbers.reduce((accumulator, currentValue) => {
-    // Check for overflow before multiplying
-    if (Number.isFinite(accumulator * currentValue)) {
-      return accumulator * currentValue;
-    } else {
-      // Handle overflow gracefully, for example, return Infinity
-      return Infinity;
-    }
-  }, 1); // Initialize accumulator with 1
+export function multiplyNumbers(numbers: number[]): number {
+  return +numbers.reduce((acc, curr) => (acc * curr), 1).toFixed(2);
+}
+
+export function convertRatesToDisplayedRates(rates: { [rate: string]: number }, base: string) {
+  const newRates = [];
+  for (const key in rates) {
+    const currentRate = rates[key];
+    const inverseRate = 1 / rates[key];
+    newRates.push([base, key, currentRate]);
+    newRates.push([key, base, inverseRate]);
+  }
+  return newRates;
 }
