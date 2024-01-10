c63668805015cf0f9fd0b08fd2d00b18418fe1b1
Adham-Niazy <adhamniazy@gmail.com>
2024-01-10 17:55:01 +0200

    Most Popular Component Logic Update

--- src/app/components/most-popular-currencies/most-popular-currencies.component.ts
+++ src/app/components/most-popular-currencies/most-popular-currencies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { POPULAR_CURRENCIES } from 'src/app/core/constants/popular_currencies';
+import { CurrencyConverterService } from 'src/app/core/services/currency-converter.service';
 import { FixerService } from 'src/app/core/services/fixer.service';
 import { convertRatesToDisplayedRates } from 'src/app/core/utils/math';
 
@@ -9,27 +11,36 @@ import { convertRatesToDisplayedRates } from 'src/app/core/utils/math';
   styleUrls: ['./most-popular-currencies.component.scss']
 })
 export class MostPopularCurrenciesComponent implements OnInit {
+  amountSubscription: Subscription;
+
   displayed_list: (string | number)[][] = [];
-  amount = 25;
-  loading = true;
+  amount = 0;
   base: string = "EUR";
-  constructor(private API: FixerService) { }
 
-  ngOnInit(): void {
-    this.loading = true;
-    // this.API.getLatestConversionRate({
-    //   base: this.base,
-    //   symbols: POPULAR_CURRENCIES.join(",")
-    // }).subscribe((res: { rates: { [rate: string]: number } }) => {
-    //   this.displayed_list = convertRatesToDisplayedRates(res.rates, this.base);
-    //   this.loading = false;
-    // })
-
-    // CAD: 1.463048,
-    // EGP: 33.783203,
-    // GBP: 0.859989,
-    // JPY: 158.248731,
-    // USD: 1.093165
+  constructor(private API: FixerService, private currencyStore: CurrencyConverterService) {
+    this.amountSubscription = this.currencyStore.amount$.subscribe((value) => {
+      this.amount = value;
+    });
   }
 
+  ngOnInit(): void {
+    this.getLatestConversionMapping();
+  }
+
+  getLatestConversionMapping() {
+    this.API.getLatestConversionRate({
+      base: this.base,
+      symbols: ""
+    }).subscribe((res: { rates: { [rate: string]: number } }) => {
+      const popularRates: { [rate: string]: number } = {};
+      for (const currency of POPULAR_CURRENCIES) {
+        popularRates[currency] = res.rates[currency];
+      }
+      this.displayed_list = convertRatesToDisplayedRates(popularRates, this.base);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.amountSubscription.unsubscribe();
+  }
 }
