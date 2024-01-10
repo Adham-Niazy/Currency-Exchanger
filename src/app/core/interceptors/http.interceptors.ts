61f4b6ebfe582106710b25f515a75aa5cefbdee6
Adham-Niazy <adhamniazy@gmail.com>
2024-01-10 21:13:27 +0200

    Adjust Constants

--- src/app/core/interceptors/http.interceptors.ts
+++ src/app/core/interceptors/http.interceptors.ts
@@ -26,7 +26,7 @@ export class HttpConfigInterceptor implements HttpInterceptor {
       headers: new HttpHeaders({ ...headers }),
       url: `${request.url.indexOf("assets") !== -1 ? '' : baseUrl}/${request.url}`,
       params: (request.params ? request.params : new HttpParams())
-        .set('access_key', 'a7db546f46e33918a665d13772c30c35')
+        .set('access_key', '3f2146b35b1e40b93e11bf4e671a2860')
     })
     return next.handle(newRequest).pipe(
       map((event: HttpEvent<any>) => {
