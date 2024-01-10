6c57c10b33b3f4b11aa235f1bdef533664eca381
Adham-Niazy <adhamniazy@gmail.com>
2024-01-10 17:55:57 +0200

    Update Fixer Service Logic

--- src/app/core/services/fixer.service.ts
+++ src/app/core/services/fixer.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, of } from 'rxjs';
 import { GET_FROM_STORAGE, SAVE_TO_STORAGE } from '../utils/storage';
-import { SYMBOLS } from '../constants/storage';
-import { isOneDayPassed } from '../utils/date';
+import { RATES, SYMBOLS } from '../constants/storage';
+import { isAmountTimePassed } from '../utils/date';
 
 @Injectable({
   providedIn: 'root'
@@ -26,11 +26,27 @@ export class FixerService {
     );
   }
 
+  private getLatesRatesFromFixer(params: { base: string, symbols: string }) {
+    return this.http.get('latest', {
+      params
+    }).pipe(
+      map((data: any) => {
+        // Store the data in localStorage along with the current date
+        const dataToStore = {
+          data,
+          lastRequestDate: new Date().toISOString(),
+        };
+        SAVE_TO_STORAGE(RATES, dataToStore)
+        return data;
+      }),
+    );
+  }
+
   getSymbolsList() {
     const savedData = GET_FROM_STORAGE(SYMBOLS);
     if (savedData.lastRequestDate) {
       // Check if one day has passed since the last request.
-      if (isOneDayPassed(savedData.lastRequestDate)) {
+      if (isAmountTimePassed(savedData.lastRequestDate, 24 * 60 * 60 * 1000)) {
         // If one day has passed, we will make a new request.
         return this.getSymbolsListFromFixer();
       } else {
@@ -44,8 +60,19 @@ export class FixerService {
   }
 
   getLatestConversionRate(params: { base: string, symbols: string }): Observable<any> {
-    return this.http.get('latest', {
-      params
-    })
+    const savedData = GET_FROM_STORAGE(RATES);
+    if (savedData.lastRequestDate) {
+      // Check if one day has passed since the last request.
+      if (isAmountTimePassed(savedData.lastRequestDate, 60 * 60 * 1000)) {
+        // If one day has passed, we will make a new request.
+        return this.getLatesRatesFromFixer(params);
+      } else {
+        // If not, return stored data as Observable.
+        return of(savedData.data);
+      }
+    } else {
+      // If no data is stored, make a new request
+      return this.getLatesRatesFromFixer(params);
+    }
   }
 }
